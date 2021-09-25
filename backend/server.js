8baa97d7f7003eb5c90ffcde40294ede4ccbaa46
goldentroll <Kcjjos@outlook.com>
2021-09-25 09:25:20 +0330

    separate backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// import http from 'http';
-// import { Server } from 'socket.io';
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -51,10 +49,9 @@ app.get('/api/config/google', (req, res) => {
   res.send({ key: process.env.GOOGLE_API_KEY || '' });
 });
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/dist/frontend')));
+app.use(express.static(path.join(__dirname, './frontend/dist/frontend')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/dist/frontend/index.html'))
+  res.sendFile(path.join(__dirname, './frontend/dist/frontend/index.html'))
 );
 
 app.use((err, req, res, next) => {
@@ -66,79 +63,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
-
-// const httpServer = http.Server(app);
-// const io = new Server(httpServer, { cors: { origin: '*' } });
-// const users = [];
-
-// io.on('connection', (socket) => {
-//   console.log('connection', socket.id);
-//   socket.on('disconnect', () => {
-//     const user = users.find((x) => x.socketId === socket.id);
-//     if (user) {
-//       user.online = false;
-//       console.log('Offline', user.name);
-//       const admin = users.find((x) => x.isAdmin && x.online);
-//       if (admin) {
-//         io.to(admin.socketId).emit('updateUser', user);
-//       }
-//     }
-//   });
-//   socket.on('onLogin', (user) => {
-//     const updatedUser = {
-//       ...user,
-//       online: true,
-//       socketId: socket.id,
-//       messages: [],
-//     };
-//     const existUser = users.find((x) => x._id === updatedUser._id);
-//     if (existUser) {
-//       existUser.socketId = socket.id;
-//       existUser.online = true;
-//     } else {
-//       users.push(updatedUser);
-//     }
-//     console.log('Online', user.name);
-//     const admin = users.find((x) => x.isAdmin && x.online);
-//     if (admin) {
-//       io.to(admin.socketId).emit('updateUser', updatedUser);
-//     }
-//     if (updatedUser.isAdmin) {
-//       io.to(updatedUser.socketId).emit('listUsers', users);
-//     }
-//   });
-
-//   socket.on('onUserSelected', (user) => {
-//     const admin = users.find((x) => x.isAdmin && x.online);
-//     if (admin) {
-//       const existUser = users.find((x) => x._id === user._id);
-//       io.to(admin.socketId).emit('selectUser', existUser);
-//     }
-//   });
-
-//   socket.on('onMessage', (message) => {
-//     if (message.isAdmin) {
-//       const user = users.find((x) => x._id === message._id && x.online);
-//       if (user) {
-//         io.to(user.socketId).emit('message', message);
-//         user.messages.push(message);
-//       }
-//     } else {
-//       const admin = users.find((x) => x.isAdmin && x.online);
-//       if (admin) {
-//         io.to(admin.socketId).emit('message', message);
-//         const user = users.find((x) => x._id === message._id && x.online);
-//         user.messages.push(message);
-//       } else {
-//         io.to(socket.id).emit('message', {
-//           name: 'Admin',
-//           body: 'Sorry. I am not online right now',
-//         });
-//       }
-//     }
-//   });
-// });
-
-// httpServer.listen(port, () => {
-//   console.log(`Serve at http://localhost:${port}`);
-// });
